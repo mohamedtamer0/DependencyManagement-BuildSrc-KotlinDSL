6959700ace584d119c9df8c96a4b618c21eee8f1
mohamedtamer0 <mt4452242@gmail.com>
2022-02-06 00:17:45 +0200

    Initial commit

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,14 @@
+/**
+ * To define dependencies
+ */
+
+object Deps {
+    val coreKts = "androidx.core:core-ktx:${Versions.coreKts}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val material = "com.google.android.material:material:${Versions.material}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val jUnit = "junit:junit:${Versions.jUnit}"
+    val jUnitExt = "androidx.test.ext:junit:${Versions.jUnitExt}"
+    val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val coreKts = "1.7.0"
+    const val appCompat = "1.4.1"
+    const val material = "1.5.0"
+    const val constraintLayout = "2.1.3"
+    const val jUnit = "4.13.2"
+    const val jUnitExt = "1.1.3"
+    const val espresso = "3.4.0"
+}
+
+
+
+
